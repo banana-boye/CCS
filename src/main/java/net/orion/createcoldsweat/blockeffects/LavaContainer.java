6bb80444b6b12fb15d3fe29c12dae5dc4d482562
banana-boye <bferraz@gmail.com>
2025-01-22 21:04:55 +0100

    rargh

--- src/main/java/net/orion/createcoldsweat/blockeffects/LavaContainer.java
+++ src/main/java/net/orion/createcoldsweat/blockeffects/LavaContainer.java
@@ -2,26 +2,33 @@ package net.orion.createcoldsweat.blockeffects;
 
 import com.momosoftworks.coldsweat.api.temperature.block_temp.BlockTemp;
 import com.simibubi.create.AllBlocks;
+import com.simibubi.create.content.fluids.pipes.FluidPipeBlock;
+import com.simibubi.create.content.fluids.pipes.FluidPipeBlockEntity;
+import com.simibubi.create.content.fluids.tank.FluidTankBlock;
 import com.simibubi.create.content.fluids.tank.FluidTankBlockEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.fluids.FluidStack;
-import net.orion.createcoldsweat.Config;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.fluids.capability.IFluidHandler;
+import net.minecraftforge.fluids.capability.templates.FluidTank;
 import net.orion.createcoldsweat.utils.HeatUtils;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BiFunction;
 
 public class LavaContainer extends BlockTemp {
 
     public LavaContainer() {
         super(
-                AllBlocks.FLUID_TANK.get(),
-                AllBlocks.FLUID_PIPE.get(),
-                AllBlocks.GLASS_FLUID_PIPE.get(),
-                AllBlocks.SMART_FLUID_PIPE.get()
+                AllBlocks.FLUID_PIPE.get()
+//                AllBlocks.GLASS_FLUID_PIPE.get(),
+//                AllBlocks.SMART_FLUID_PIPE.get(),
+//                AllBlocks.BASIN.get()
         );
     }
 
@@ -29,13 +36,10 @@ public class LavaContainer extends BlockTemp {
 
     @Override
     public double getTemperature(Level level, @Nullable LivingEntity livingEntity, BlockState blockState, BlockPos blockPos, double distance) {
-//        if (!Config.CONFIG.lavaTemperature.get()) return 0d;
-//        FluidTankBlockEntity blockEntity = (FluidTankBlockEntity) level.getBlockEntity(blockPos);
-//        if (this.hasBlock(blockState.getBlock()) && blockEntity != null){
-//            FluidStack fluidStack = blockEntity.getFluid(1);
-//            if (fluidStack.containsFluid(HeatUtils.LAVA))
-//                return lavaBlend.apply(distance, Config.CONFIG.lavaTemperatureIncrement.get() * fluidStack.getAmount());
-//        }
+        BlockEntity blockEntity = level.getBlockEntity(blockPos);
+        if (!this.hasBlock(blockState.getBlock()) || blockEntity == null) return 0d;
+        FluidPipeBlockEntity pipeBlockEntity = (FluidPipeBlockEntity) blockEntity;
+        System.out.println("Pipe NBT:" + pipeBlockEntity.getBehaviour(Standard));
         return 0d;
     }
 }
